b263cc01f2347d19d6d731b8b4c282bfc8dbd61b
maaqib90 <maaqib90#gmail.com>
2023-07-27 13:19:40 +0530

    done

--- app.py
+++ app.py
@@ -6,8 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    img_list = ['boy.jpg', 'girl.jpg', 'landscape.jpg']  # Add actual image names here
+    img_list = get_available_images()
     return render_template('index.html', img_list=img_list)
+
+def get_available_images():
+    # Get a list of available image names from the source_image folder
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    source_image_folder = os.path.join(current_directory, 'examples', 'source_image')
+    img_list = [img_name.split('.')[0] for img_name in os.listdir(source_image_folder) if img_name.endswith('.png')]
+    return img_list
 
 @app.route('/generate_animation', methods=['POST'])
 def generate_animation_endpoint():
@@ -15,11 +22,14 @@ def generate_animation_endpoint():
         image_name = request.form['image_name']
         audio_file = request.files['audio']
 
-        if image_name and audio_file:
-            animation_url = generate_animation(image_name, audio_file)
-            return jsonify({'animation_url': animation_url})
-        else:
+        if not image_name or not audio_file:
             return jsonify({'error': 'Please select both an image and an audio file.'})
+
+        if image_name not in get_available_images():
+            return jsonify({'error': 'Invalid image selected. Please choose from the available images.'})
+
+        animation_url = generate_animation(image_name, audio_file)
+        return jsonify({'animation_url': animation_url})
 
 if __name__ == '__main__':
     app.run(debug=True)
