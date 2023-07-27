a10deb6f4f2770c078d8dc68fdb748d932ebff0b
maaqib90 <maaqib90#gmail.com>
2023-07-27 13:36:21 +0530

    done

--- inference.py
+++ inference.py
@@ -1,17 +1,16 @@
 import glob
 import os
-import subprocess
 from base64 import b64encode
+from inference import your_inference_function  # Replace 'your_inference_function' with the actual function name from inference.py
 
 def generate_animation(image_name, audio_file):
     # Assuming the audio file is saved in the 'driven_audio' folder
     audio_file.save(f'examples/driven_audio/{audio_file.filename}')
 
     img = f'examples/source_image/{image_name}.png'
-    subprocess.run([
-        'python3.12', 'inference.py', '--driven_audio', f'examples/driven_audio/{audio_file.filename}',
-        '--source_image', img, '--result_dir', './results', '--still', '--preprocess', 'full', '--enhancer', 'gfpgan'
-    ])
+
+    # Call the inference function directly instead of using subprocess.run()
+    your_inference_function(audio_file.filename, img)
 
     results = sorted(os.listdir('./results/'))
     mp4_name = glob.glob('./results/*.mp4')[0]
