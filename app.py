1d07ee191bfb60133299b826041656e7bf3c1e2a
maaqib90 <maaqib90#gmail.com>
2023-07-27 13:33:42 +0530

    done

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
 from flask import Flask, render_template, request, jsonify
-import os
 from inference import generate_animation
 
 app = Flask(__name__)
 
-def get_available_images():
-    # Get the absolute path of the current script's directory
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-
-    # Get the absolute path of the 'SadTalker_Flask_App' directory
-    app_directory = os.path.abspath(os.path.join(current_directory, os.pardir))
-
-    # Get the absolute path of the 'examples/source_image' folder
-    source_image_folder = os.path.join(app_directory, 'examples', 'source_image')
-
-    # List all image names in the folder with .jpg extension
-    img_list = [img_name.split('.')[0] for img_name in os.listdir(source_image_folder) if img_name.endswith('.jpg')]
-    return img_list
-
 @app.route('/')
 def home():
-    img_list = get_available_images()
+    img_list = ['boy.jpg', '...']  # List of available image names, you need to add the actual names
     return render_template('index.html', img_list=img_list)
 
 @app.route('/generate_animation', methods=['POST'])
@@ -28,12 +13,8 @@ def generate_animation_endpoint():
     if request.method == 'POST':
         image_name = request.form['image_name']
         audio_file = request.files['audio']
-
-        if image_name and audio_file:
-            animation_url = generate_animation(image_name, audio_file)
-            return jsonify({'animation_url': animation_url})
-        else:
-            return jsonify({'error': 'Please select both an image and an audio file.'})
+        animation_url = generate_animation(image_name, audio_file)
+        return jsonify({'animation_url': animation_url})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- inference.py
+++ inference.py
@@ -2,50 +2,21 @@ import glob
 import os
 import subprocess
 from base64 import b64encode
-import sys
 
 def generate_animation(image_name, audio_file):
-    # Get the absolute path of the current script's directory
-    current_directory = os.path.dirname(os.path.abspath(__file__))
+    # Assuming the audio file is saved in the 'driven_audio' folder
+    audio_file.save(f'examples/driven_audio/{audio_file.filename}')
 
-    # Get the absolute path of the 'SadTalker_Flask_App' directory
-    app_directory = os.path.abspath(os.path.join(current_directory, os.pardir))
+    img = f'examples/source_image/{image_name}.png'
+    subprocess.run([
+        'python3.12', 'inference.py', '--driven_audio', f'examples/driven_audio/{audio_file.filename}',
+        '--source_image', img, '--result_dir', './results', '--still', '--preprocess', 'full', '--enhancer', 'gfpgan'
+    ])
 
-    # Create the 'driven_audio' folder if it doesn't exist
-    driven_audio_folder = os.path.join(app_directory, 'examples', 'driven_audio')
-    os.makedirs(driven_audio_folder, exist_ok=True)
+    results = sorted(os.listdir('./results/'))
+    mp4_name = glob.glob('./results/*.mp4')[0]
 
-    # Save the audio file to the 'driven_audio' folder
-    audio_file_path = os.path.join(driven_audio_folder, audio_file.filename)
-    audio_file.save(audio_file_path)
-
-    # Get the absolute path of the image
-    img_path = os.path.join(app_directory, 'examples', 'source_image', f'{image_name}.jpg')
-
-    # Specify the command to run based on the Python interpreter in the environment
-    python_interpreter = sys.executable
-    command = [
-        python_interpreter, 'inference.py', '--driven_audio', audio_file_path,
-        '--source_image', img_path, '--result_dir', os.path.join(app_directory, 'results'),
-        '--still', '--preprocess', 'full', '--enhancer', 'gfpgan'
-    ]
-
-    # Create the 'results' folder if it doesn't exist
-    results_folder = os.path.join(app_directory, 'results')
-    os.makedirs(results_folder, exist_ok=True)
-
-    # Run the inference command
-    subprocess.run(command)
-
-    # Get the generated animation
-    results = sorted(os.listdir(results_folder))
-    mp4_files = glob.glob(os.path.join(results_folder, '*.mp4'))
-    if not mp4_files:
-        raise ValueError('No animation files were generated.')
-
-    mp4_name = mp4_files[0]
-    # Get the base64 encoded video data
-    mp4 = open(mp4_name, 'rb').read()
+    mp4 = open(f'{mp4_name}', 'rb').read()
     animation_url = "data:video/mp4;base64," + b64encode(mp4).decode()
 
     return animation_url
