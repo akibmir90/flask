3b9b3783af9077f59a8dbce542131bf9f63f104f
maaqib90 <maaqib90#gmail.com>
2023-07-27 13:23:55 +0530

    done

--- inference.py
+++ inference.py
@@ -8,30 +8,30 @@ def generate_animation(image_name, audio_file):
     # Get the absolute path of the current script's directory
     current_directory = os.path.dirname(os.path.abspath(__file__))
 
+    # Get the absolute path of the 'SadTalker_Flask_App' directory
+    app_directory = os.path.abspath(os.path.join(current_directory, os.pardir))
+
     # Create the 'driven_audio' folder if it doesn't exist
-    driven_audio_folder = os.path.join(current_directory, 'examples', 'driven_audio')
+    driven_audio_folder = os.path.join(app_directory, 'examples', 'driven_audio')
     os.makedirs(driven_audio_folder, exist_ok=True)
 
     # Save the audio file to the 'driven_audio' folder
     audio_file_path = os.path.join(driven_audio_folder, audio_file.filename)
     audio_file.save(audio_file_path)
 
-    # Get the absolute path of the 'examples' folder
-    examples_directory = os.path.abspath(os.path.join(current_directory, 'examples'))
-
     # Get the absolute path of the image
-    img_path = os.path.join(examples_directory, 'source_image', f'{image_name}.png')
+    img_path = os.path.join(app_directory, 'examples', 'source_image', f'{image_name}.png')
 
     # Specify the command to run based on the Python interpreter in the environment
     python_interpreter = sys.executable
     command = [
         python_interpreter, 'inference.py', '--driven_audio', audio_file_path,
-        '--source_image', img_path, '--result_dir', os.path.join(examples_directory, 'results'),
+        '--source_image', img_path, '--result_dir', os.path.join(app_directory, 'results'),
         '--still', '--preprocess', 'full', '--enhancer', 'gfpgan'
     ]
 
     # Create the 'results' folder if it doesn't exist
-    results_folder = os.path.join(examples_directory, 'results')
+    results_folder = os.path.join(app_directory, 'results')
     os.makedirs(results_folder, exist_ok=True)
 
     # Run the inference command
