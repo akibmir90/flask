64295b0887007882d74f599068eca8f08c43fcbc
maaqib90 <maaqib90#gmail.com>
2023-07-27 14:19:49 +0530

    changes done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from inference import generate_animation
 from common import your_inference_function
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    img_list = ['boy.jpg', 'girl.jpg']  # List of available image names, you need to add the actual names
+    img_list = ['boy', '...']  # List of available image names, you need to add the actual names
     return render_template('index.html', img_list=img_list)
 
 @app.route('/generate_animation', methods=['POST'])
@@ -15,7 +15,15 @@ def generate_animation_endpoint():
         image_name = request.form['image_name']
         audio_file = request.files['audio']
         animation_url = generate_animation(image_name, audio_file)
-        return jsonify({'animation_url': animation_url})
+        if animation_url:
+            return redirect(url_for('show_animation', animation_url=animation_url))
+        else:
+            return jsonify({'animation_url': None})
+
+@app.route('/animation')
+def show_animation():
+    animation_url = request.args.get('animation_url')
+    return render_template('animation.html', animation_url=animation_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
