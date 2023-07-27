bb02e0a3c0daa0f96bbeedfe6de297088c961991
maaqib90 <maaqib90#gmail.com>
2023-07-27 13:30:05 +0530

    done

--- app.py
+++ app.py
@@ -4,17 +4,21 @@ from inference import generate_animation
 
 app = Flask(__name__)
 
+def get_available_images():
+    # Get the absolute path of the current script's directory
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+
+    # Get the absolute path of the 'examples/source_image' folder
+    source_image_folder = os.path.join(current_directory, 'examples', 'source_image')
+
+    # List all image names in the folder with .jpg extension
+    img_list = [img_name.split('.')[0] for img_name in os.listdir(source_image_folder) if img_name.endswith('.jpg')]
+    return img_list
+
 @app.route('/')
 def home():
     img_list = get_available_images()
     return render_template('index.html', img_list=img_list)
-
-def get_available_images():
-    # Get a list of available image names from the source_image folder
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    source_image_folder = os.path.join(current_directory, 'examples', 'source_image')
-    img_list = [img_name.split('.')[0] for img_name in os.listdir(source_image_folder) if img_name.endswith('.jpg')]
-    return img_list
 
 @app.route('/generate_animation', methods=['POST'])
 def generate_animation_endpoint():
@@ -22,14 +26,11 @@ def generate_animation_endpoint():
         image_name = request.form['image_name']
         audio_file = request.files['audio']
 
-        if not image_name or not audio_file:
+        if image_name and audio_file:
+            animation_url = generate_animation(image_name, audio_file)
+            return jsonify({'animation_url': animation_url})
+        else:
             return jsonify({'error': 'Please select both an image and an audio file.'})
-
-        if image_name not in get_available_images():
-            return jsonify({'error': 'Invalid image selected. Please choose from the available images.'})
-
-        animation_url = generate_animation(image_name, audio_file)
-        return jsonify({'animation_url': animation_url})
 
 if __name__ == '__main__':
     app.run(debug=True)
